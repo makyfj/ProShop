0ac276e2e0a1d6f13107d79d43eacc821e52d7f2
Maky <franklinjaradev@gmail.com>
2021-05-05 16:23:05 -0400

    Added custom error handling

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from "colors";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -10,11 +11,23 @@ connectDB();
 
 const app = express();
 
+// middleware example
+// app.use((req, res, next) => {
+//   console.log(req.originalUrl);
+//   next();
+// });
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
 app.use("/api/products", productRoutes);
+
+// Not found
+app.use(notFound);
+
+// Error handling
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
