0392b6daa5f8ad362eeb4323cd3f0f5048f98e65
Maky <franklinjaradev@gmail.com>
2021-05-04 20:54:42 -0400

    Added express and json dummy data

---
+++ backend/data/products.js
@@ -0,0 +1,83 @@
+const products = [
+  {
+    _id: "1",
+    name: "Airpods",
+    image: "/images/airpods.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    _id: "2",
+    name: "Alexa",
+    image: "/images/alexa.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality",
+    brand: "Amazon",
+    category: "Electronics",
+    price: 99.99,
+    countInStock: 0,
+    rating: 4.5,
+    numReviews: 2,
+  },
+
+  {
+    _id: "3",
+    name: "camera",
+    image: "/images/camera.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 3,
+    rating: 2.5,
+    numReviews: 3,
+  },
+  {
+    _id: "4",
+    name: "iPhone",
+    image: "/images/phone.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 3,
+    rating: 3.5,
+    numReviews: 8,
+  },
+  {
+    _id: "5",
+    name: "Mouse",
+    image: "/images/mouse.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 3,
+    rating: 1.5,
+    numReviews: 8,
+  },
+  {
+    _id: "6",
+    name: "PlayStation",
+    image: "/images/playstation.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality",
+    brand: "Sony",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 3,
+    rating: 2.5,
+    numReviews: 2,
+  },
+];
+
+module.exports = products;
