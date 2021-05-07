b6dc030368e628a1518bf34c6c223c18f096d311
Maky <franklinjaradev@gmail.com>
2021-05-06 22:48:38 -0400

    User Authentication Endpoint

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from "colors";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -11,17 +12,14 @@ connectDB();
 
 const app = express();
 
-// middleware example
-// app.use((req, res, next) => {
-//   console.log(req.originalUrl);
-//   next();
-// });
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 // Not found
 app.use(notFound);
