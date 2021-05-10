85ad521e98cb3afc18a0c84cfa9acd95a3eb2a1c
Maky <franklinjaradev@gmail.com>
2021-05-10 17:55:46 -0400

    Added front-end image upload

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,7 +1,6 @@
 import path from "path";
 import express from "express";
 import multer from "multer";
-
 const router = express.Router();
 
 const storage = multer.diskStorage({
@@ -16,27 +15,25 @@ const storage = multer.diskStorage({
   },
 });
 
-const checkFileType = (file, cb) => {
-  const fileTypes = /jpg|jpeg|png/;
-  const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
-  const mineType = fileTypes.test(file.mineType);
+function checkFileType(file, cb) {
+  const filetypes = /jpg|jpeg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
 
-  if (extName && mineType) {
+  if (extname && mimetype) {
     return cb(null, true);
   } else {
     cb("Images only!");
   }
-};
+}
 
 const upload = multer({
   storage,
   fileFilter: function (req, file, cb) {
-    // Checks only for jpg, jpeg, png
-    checkFileType();
+    checkFileType(file, cb);
   },
 });
 
-// endpoint
 router.post("/", upload.single("image"), (req, res) => {
   res.send(`/${req.file.path}`);
 });
